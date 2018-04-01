64c3c844c5acd78c0d3b00c7fcc3b5e009be4dbb
Bill Clerico <clerico@us.ibm.com>
2018-04-01 09:33:04 -0400

    fix api key issue

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ $_SESSION['db_port'] = $ini_array['db_port'];
 $_SESSION['db_user_id'] = $ini_array['db_user_id'];
 $_SESSION['db_password'] = $ini_array['db_password'];
 $_SESSION['db_name'] = $ini_array['db_name'];
+$_SESSION['google_maps_api_key'] = $ini_array['google_maps_api_key'];
 
 header("Location: ".$_SESSION['goto']);
 ?>
