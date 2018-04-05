325740cd32e5a1154af2c185901997e135169772
Bill Clerico <clerico@us.ibm.com>
2018-04-05 16:24:45 -0400

    retrying structure repair

--- footer.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "<span class='footerText'>Created and maintained by Bill Clerico (<a href='mailto:bclerico@gmail.com'>bclerico@gmail.com</a>) - All Rights Reserved - Copyright 2018</span>"
-?>
--- header.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "<h1>Watch My Spot - Global Expedition Observation Portal</h1>"
-?>
